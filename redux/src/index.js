58c95ee0a3c90a7f84a454330f981676ec2d5038
Fayecodeur <fayecodeur99@gmail.com>
2025-08-16 13:42:35 +0000

    finition du cours

--- redux/src/index.js
+++ redux/src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/index.scss";
-
+import { store } from "./store.js";
+import Provider from "react-redux";
 ReactDOM.render(<App />, document.getElementById("root"));
