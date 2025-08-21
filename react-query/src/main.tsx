aaa23d5b0bce49a80669b310ebacdf87dc71a3c8
Fayecodeur <fayecodeur99@gmail.com>
2025-08-21 11:38:46 +0000

    Gestion de la navigation

--- react-query/src/main.tsx
+++ react-query/src/main.tsx
@@ -2,12 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>
 );
