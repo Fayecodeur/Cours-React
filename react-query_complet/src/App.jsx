d90ce0940b4503df9d7e70bfd6313d2cd4f8ab87
Fayecodeur <fayecodeur99@gmail.com>
2025-08-23 15:49:56 +0000

    Gestion des routes

--- react-query_complet/src/App.jsx
+++ react-query_complet/src/App.jsx
@@ -3,16 +3,20 @@ import "./App.css";
 import Navbar from "./components/Navbar.jsx";
 import Home from "./pages/Home.jsx";
 import Authors from "./pages/Authors.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
-    <>
+    <div className="flex flex-col h-screen">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/authors" element={<Authors />} />
-      </Routes>
-    </>
+      <main className="grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/authors" element={<Authors />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
