b3fa56e93e917bfcd26ef61df5a897e97e91cb41
Fayecodeur <fayecodeur99@gmail.com>
2025-08-23 18:40:25 +0000

    Composant pour affichage des posts

--- react-query_complet/src/components/Posts.jsx
+++ react-query_complet/src/components/Posts.jsx
@@ -1,9 +1,19 @@
-export default function Posts() {
+import { Link } from "react-router-dom";
+
+export default function Posts({ title, body, path }) {
+  function trucate(str, length) {
+    return str.substr(0, length) + "...";
+  }
   return (
-    <section className="py-10">
-      <h1 className="text-center mb-5 text-2xl md:text-3xl">
-        Liste des articles
-      </h1>
-    </section>
+    <div className="p-6 shadow hover:shadow-md">
+      <h2 className="text-xl mb-3">{trucate(title, 30)}</h2>
+      <p className="text-gray-500 mb-5">{trucate(body, 100)}</p>
+      <Link
+        to={path}
+        className="bg-blue-500 block w-[100px] text-white py-2 px-3 rounded-md hover:bg-blue-600 text-center"
+      >
+        Voir plus
+      </Link>
+    </div>
   );
 }
