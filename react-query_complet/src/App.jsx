f1b495ac80f8929dc7a4ad3396a07741a7450c16
Fayecodeur <fayecodeur99@gmail.com>
2025-08-23 13:52:09 +0000

    gestion de la navigation

--- react-query_complet/src/App.jsx
+++ react-query_complet/src/App.jsx
@@ -1,7 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar.jsx";
+import Home from "./pages/Home.jsx";
+import Authors from "./pages/Authors.jsx";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/authors" element={<Authors />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
