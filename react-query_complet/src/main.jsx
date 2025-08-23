baa1ccb0799cd9c2f8ae2df9acf538b17bdc3a05
Fayecodeur <fayecodeur99@gmail.com>
2025-08-23 15:50:41 +0000

    Mise en place react-query

--- react-query_complet/src/main.jsx
+++ react-query_complet/src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+const client = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={client}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 );
