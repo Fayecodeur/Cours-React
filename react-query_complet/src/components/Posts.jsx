4cb26699c3500c6cd871a48b43a9131f6250c531
Fayecodeur <fayecodeur99@gmail.com>
2025-08-23 15:46:20 +0000

    Création du composant post

---
+++ react-query_complet/src/components/Posts.jsx
@@ -0,0 +1,9 @@
+export default function Posts() {
+  return (
+    <section className="py-10">
+      <h1 className="text-center mb-5 text-2xl md:text-3xl">
+        Liste des articles
+      </h1>
+    </section>
+  );
+}
