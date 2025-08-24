38ffbd8df9fdfbb85ed88f751b73377f8815f626
Fayecodeur <fayecodeur99@gmail.com>
2025-08-24 19:15:00 +0000

    Ajout du devtools

--- react-query_complet/src/main.jsx
+++ react-query_complet/src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const client = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={client}>
         <App />
+        <ReactQueryDevtools />
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
