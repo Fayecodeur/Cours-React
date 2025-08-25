f7a62b13702878c972846fb27457c938aa0cc56f
Fayecodeur <fayecodeur99@gmail.com>
2025-08-25 19:23:27 +0000

    affichage d'un seul article

--- react-query_complet/src/pages/PostDetails.jsx
+++ react-query_complet/src/pages/PostDetails.jsx
@@ -1,5 +1,22 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import usePost from "../services/usePost.js";
+
 export default function PostDetails() {
   const { id } = useParams();
-  return <div>PostDetails {id}</div>;
+  const { data: post, isLoading, error } = usePost(id);
+  if (isLoading) return <p>Chargement...</p>;
+  if (error) return <p>Erreur : {error.message}</p>;
+
+  return (
+    <div className="py-10 container mx-auto px-6 md:px-8">
+      <h2 className="mb-3 text-2xl">{post.title}</h2>
+      <p className="text-gray-800">{post.body}</p>
+      <Link
+        to="/"
+        className="bg-blue-500 text-white font-bold px-3 py-2 rounded-md mt-5 block w-[100px] text-center"
+      >
+        Retour
+      </Link>
+    </div>
+  );
 }
