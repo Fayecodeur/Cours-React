5c02ad6b72fe7c23ae773960c9897af86605f2bf
Demba Faye <fayecodeur99@gmail.com>
2026-02-13 16:11:13 +0000

    Recupération des données avec react query

--- learn-react-query-project/src/pages/Users.jsx
+++ learn-react-query-project/src/pages/Users.jsx
@@ -1,35 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useEffect, useState } from "react";
 
 export function Users() {
-  const [authors, setAuthors] = useState([]);
+  const fetchAuthors = async () => {
+    const response = await axios.get("http://localhost:3000/users");
+    return response.data;
+  };
 
-  useEffect(() => {
-    async function fetchAuthors() {
-      try {
-        const response = await axios.get('http://localhost:3000/users');
-        setAuthors(response.data)
-      } catch (error) {
-        console.error(error);
-      }
-    }
-
-    fetchAuthors();
-  }, []);
+  const {
+    data: authors,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ["authors"],
+    queryFn: fetchAuthors,
+  });
 
   return (
     <div>
       <h1 className="text-4xl my-8 text-center">Auteurs</h1>
+      {isLoading && <p className="text-center">Chargement</p>}
+      {error && <p className="text-center text-red-500">Erreur</p>}
       <ul>
-        {
-          authors.map((author) => (
-            <li key={author.id} className="flex flex-col my-4">
-              <span>{author.name} <span className="text-gray-400">@{author.username}</span></span>
-              <span className="text-gray-500">{author.email}</span>
-            </li>
-          ))
-        }
+        {authors?.map((author) => (
+          <li key={author.id} className="flex flex-col my-4">
+            <span>
+              {author.name}{" "}
+              <span className="text-gray-400">@{author.username}</span>
+            </span>
+            <span className="text-gray-500">{author.email}</span>
+          </li>
+        ))}
       </ul>
     </div>
-  )
+  );
 }
