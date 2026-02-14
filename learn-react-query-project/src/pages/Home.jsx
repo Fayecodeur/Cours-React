bb5e98106335f9e23d09e04551f5fef304e5069a
Demba Faye <fayecodeur99@gmail.com>
2026-02-14 19:04:19 +0000

    Recupération des articles

--- learn-react-query-project/src/pages/Home.jsx
+++ learn-react-query-project/src/pages/Home.jsx
@@ -1,48 +1,41 @@
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 import { Post } from "../components/Posts/Post";
 
 export function Home() {
+  const getPosts = async () => {
+    const response = await axios.get("http://localhost:3000/posts?_limit=20");
+    return response.data;
+  };
+
+  const {
+    data: posts,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["posts"],
+    queryFn: getPosts,
+  });
+
   return (
-    <>
-    <h1 className="text-5xl text-center my-10">Articles</h1>
-     <div className="flex flex-wrap -mx-8 py-10">
-      <div className="w-1/3 px-8 my-8">
-        <Post
-          title="Lorem ipsum dolor sit"
-          description="Deserunt similique assumenda saepe ipsum nobis quos cupiditate excepturi ab atque fugit facilis deleniti eveniet veniam nostrum harum, provident esse autem sunt?"
-        />
-      </div>
-      <div className="w-1/3 px-8 my-8">
-        <Post
-          title="Lorem ipsum dolor sit"
-          description="Deserunt similique assumenda saepe ipsum nobis quos cupiditate excepturi ab atque fugit facilis deleniti eveniet veniam nostrum harum, provident esse autem sunt?"
-        />
-      </div>
-      <div className="w-1/3 px-8 my-8">
-        <Post
-          title="Lorem ipsum dolor sit"
-          description="Deserunt similique assumenda saepe ipsum nobis quos cupiditate excepturi ab atque fugit facilis deleniti eveniet veniam nostrum harum, provident esse autem sunt?"
-        />
-      </div>
-      <div className="w-1/3 px-8 my-8">
-        <Post
-          title="Lorem ipsum dolor sit"
-          description="Deserunt similique assumenda saepe ipsum nobis quos cupiditate excepturi ab atque fugit facilis deleniti eveniet veniam nostrum harum, provident esse autem sunt?"
-        />
-      </div>
-      <div className="w-1/3 px-8 my-8">
-        <Post
-          title="Lorem ipsum dolor sit"
-          description="Deserunt similique assumenda saepe ipsum nobis quos cupiditate excepturi ab atque fugit facilis deleniti eveniet veniam nostrum harum, provident esse autem sunt?"
-        />
-      </div>
-      <div className="w-1/3 px-8 my-8">
-        <Post
-          title="Lorem ipsum dolor sit"
-          description="Deserunt similique assumenda saepe ipsum nobis quos cupiditate excepturi ab atque fugit facilis deleniti eveniet veniam nostrum harum, provident esse autem sunt?"
-        />
+    <div className="max-w-7xl mx-auto px-6">
+      <h1 className="text-4xl font-bold text-center my-10">Articles</h1>
+
+      {isLoading && (
+        <p className="text-center text-lg">Chargement des articles...</p>
+      )}
+
+      {error && (
+        <p className="text-center text-red-500 text-lg">
+          Erreur lors de la récupération des posts
+        </p>
+      )}
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 py-10">
+        {posts?.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
       </div>
     </div>
-    </>
-
-  )
+  );
 }
