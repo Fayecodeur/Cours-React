dfa9a887d49e3cc7df2cdeb0bda5b2bb1d25dc65
Demba Faye <fayecodeur99@gmail.com>
2026-02-14 19:03:56 +0000

    Affichage des articles

--- learn-react-query-project/src/components/Posts/Post.jsx
+++ learn-react-query-project/src/components/Posts/Post.jsx
@@ -1,10 +1,13 @@
-export function Post({ title, description}) {
+export function Post({ post }) {
   return (
-    <div>
-      <h3 className="text-3xl">{title}</h3>
-      <p className="text-gray-500">
-        {description}
-      </p>
+    <div className="bg-white rounded-xl shadow-md p-6 hover:shadow-xl transition">
+      <h2 className="text-xl font-semibold mb-2 line-clamp-2">{post.title}</h2>
+
+      <p className="text-gray-600 line-clamp-3 mb-4">{post.body}</p>
+
+      <button className="text-blue-600 font-medium hover:underline">
+        Lire plus
+      </button>
     </div>
-  )
+  );
 }
