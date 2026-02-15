6eb79773d0164676ddc48ba30820434eb554feaa
Demba Faye <fayecodeur99@gmail.com>
2026-02-15 16:04:40 +0000

    Suppression d'un utilisateur

--- crud/src/components/UserItems.jsx
+++ crud/src/components/UserItems.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteUser } from "../api/userApi";
+import toast from "react-hot-toast";
 
-export default function UserItems({ user: { prenom, nom, email, sexe } }) {
+export default function UserItems({ user: { id, prenom, nom, email, sexe } }) {
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: (id) => deleteUser(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["users"] });
+      toast.success("Utlisateur supprimé");
+    },
+    onError: () => {
+      toast.error("Echec lors de la suppression");
+    },
+  });
+
   return (
     <tr>
       <td>{prenom}</td>
       <td>{nom}</td>
       <td>{email}</td>
       <td>{sexe}</td>
+      <td>
+        <button className="btn btn-warning btn-sm me-3">Modifier</button>
+        <button
+          onClick={() => mutation.mutate(id)}
+          className="btn btn-danger btn-sm"
+        >
+          Supprimer
+        </button>
+      </td>
     </tr>
   );
 }
