0656107e1e2113d3eb7c5d14612c842d010ba14b
Demba Faye <fayecodeur99@gmail.com>
2026-02-15 15:12:35 +0000

    Ajout d'utilisateur

--- crud/src/api/userApi.js
+++ crud/src/api/userApi.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 const API_URL = "http://localhost:3000/users";
+
 export const getUsers = async () => {
   const res = await axios.get(API_URL);
+  return res.data;
+};
+
+export const addUser = async (user) => {
+  const res = await axios.post(API_URL, user);
   return res.data;
 };
