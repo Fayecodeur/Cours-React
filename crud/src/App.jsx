685226fa3d2d263852683422583a6f348df40f3c
Demba Faye <fayecodeur99@gmail.com>
2026-02-19 16:14:15 +0000

    Ajout du lien de modification

--- crud/src/App.jsx
+++ crud/src/App.jsx
@@ -1,9 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 import Users from "./pages/Users";
+import UserEdit from "./components/UserEdit";
 
 function App() {
   return (
     <>
-      <Users />
+      <Routes>
+        <Route path="/" element={<Users />}></Route>
+        <Route path="/edit/user/:id" element={<UserEdit />}></Route>
+      </Routes>
     </>
   );
 }
