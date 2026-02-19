2ad58898eae67c913514f7161c5721b3f9d7c658
Demba Faye <fayecodeur99@gmail.com>
2026-02-19 16:15:21 +0000

    Recupération et modification d'un user

--- crud/src/api/userApi.js
+++ crud/src/api/userApi.js
@@ -6,8 +6,18 @@ export const getUsers = async () => {
   return res.data;
 };
 
+export const getUserById = async (id) => {
+  const res = await axios.get(`${API_URL}/${id}`);
+  return res.data;
+};
+
 export const addUser = async (user) => {
   const res = await axios.post(API_URL, user);
+  return res.data;
+};
+
+export const updateUser = async (id, data) => {
+  const res = await axios.put(`${API_URL}/${id}`, data);
   return res.data;
 };
 
